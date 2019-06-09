cdb75a6fe0b37fe1760af47962313e37e6d4dbdb
Matheus Milani <m_milani@hotmail.com>
2019-06-09 19:12:31 -0300

    Exercicio4

---
+++ LabClasses/Lab1/Projeto/src/atividade1/Exercicio4.java
@@ -0,0 +1,61 @@
+package atividade1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercicio4 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Digite a posição e o valor que deseja inserir");
+    int position = scan.nextInt();
+    int value = scan.nextInt();
+
+    scan.close();
+    
+    int[] array = {25, 14, 56, 15, 36, 66, 77, 18, 29, 0};
+    System.out.println("Array inicial:");
+    System.out.println(Arrays.toString(array));
+
+    if(insert(value, position, array)){
+      System.out.println("Inserido com sucesso!");
+      System.out.println(Arrays.toString(array));
+    } else {
+      System.out.println("Não foi possivel inserir.");
+    }
+  }
+  
+  public static int[] getRandomIntArray(int length) {
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++) {
+      array[i] = (int)(Math.random() * 2);
+    }
+
+    return array;
+  }
+
+  public static boolean insert(int value, int position, int[] array) {
+    int length = array.length;
+
+    for (int i = 0; i < length; i++) {
+      if(array[i] == 0 && i >= position) {
+        shift(array, position, i);
+        array[position] = value;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static void shift(int[] array, int begin, int end) {
+    if(begin == end) return;
+    
+    for (int i = end; i > begin; i--) {
+      int temp = array[i];
+      array[i] = array[i - 1];
+      array[i -1] = temp;
+    }
+  }
+}
