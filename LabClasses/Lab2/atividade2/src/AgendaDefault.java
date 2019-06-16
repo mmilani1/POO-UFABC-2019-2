18fc31c5999bd92254bcf19b2e35a27413198120
Matheus Milani <m_milani@hotmail.com>
2019-06-16 15:44:43 -0300

    Exercicio 2

--- LabClasses/Lab2/atividade2/src/AgendaDefault.java
+++ LabClasses/Lab2/atividade2/src/AgendaDefault.java
@@ -6,18 +6,33 @@ import pessoa.*;
 public class AgendaDefault {
   public static void main(String[] args) {
     Agenda agenda = new Agenda();
+    populateAgenda(agenda); // Adding 9 pessoas
+
     Pessoa pessoa1 = new Pessoa("Nome1", "teste@teste.com", "(11)1234-5678");
+    agenda.add(pessoa1);
+
+    Pessoa pessoa11 = new Pessoa("Nome11", "teste@teste.com", "(11)1234-5678");
+    agenda.add(pessoa11); // Agenda is full already
+
+    System.out.printf("Nome: %s, Posição: %d\n", pessoa1.getName(), agenda.includes(pessoa1));
+    System.out.printf("Nome: %s, Posição: %d\n", pessoa11.getName(), agenda.includes(pessoa11));
+
+    agenda.searchEmailByName("Email");
+    agenda.searchPhoneByName("Phone");
+
+  }
+
+  private static void populateAgenda(Agenda agenda) {
     Pessoa pessoa2 = new Pessoa("Nome2", "teste@teste.com", "(11)1234-5678");
     Pessoa pessoa3 = new Pessoa("Nome3", "teste@teste.com", "(11)1234-5678");
-    Pessoa pessoa4 = new Pessoa("Nome4", "teste@teste.com", "(11)1234-5678");
+    Pessoa pessoa4 = new Pessoa("BuscarEmail", "searchedEmail@teste.com", "(11)1234-5678");
     Pessoa pessoa5 = new Pessoa("Nome5", "teste@teste.com", "(11)1234-5678");
     Pessoa pessoa6 = new Pessoa("Nome6", "teste@teste.com", "(11)1234-5678");
-    Pessoa pessoa7 = new Pessoa("Nome7", "teste@teste.com", "(11)1234-5678");
+    Pessoa pessoa7 = new Pessoa("BuscarPhone", "teste@teste.com", "(11)9999-9999");
     Pessoa pessoa8 = new Pessoa("Nome8", "teste@teste.com", "(11)1234-5678");
     Pessoa pessoa9 = new Pessoa("Nome9", "teste@teste.com", "(11)1234-5678");
     Pessoa pessoa10 = new Pessoa("Nome10", "teste@teste.com", "(11)1234-5678");
 
-    agenda.add(pessoa1);
     agenda.add(pessoa10);
     agenda.add(pessoa2);
     agenda.add(pessoa3);
@@ -27,17 +42,5 @@ public class AgendaDefault {
     agenda.add(pessoa6);
     agenda.add(pessoa4);
     agenda.add(pessoa9);
-
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa1.getName(), agenda.includes(pessoa1));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa2.getName(), agenda.includes(pessoa2));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa3.getName(), agenda.includes(pessoa3));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa4.getName(), agenda.includes(pessoa4));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa5.getName(), agenda.includes(pessoa5));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa6.getName(), agenda.includes(pessoa6));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa7.getName(), agenda.includes(pessoa7));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa8.getName(), agenda.includes(pessoa8));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa9.getName(), agenda.includes(pessoa9));
-    System.out.printf("Nome: %s, Posição: %d\n", pessoa10.getName(), agenda.includes(pessoa10));
-
   }
 }
--- LabClasses/Lab2/atividade2/src/pessoa/Agenda.java
+++ LabClasses/Lab2/atividade2/src/pessoa/Agenda.java
@@ -2,16 +2,20 @@ package pessoa;
 
 public class Agenda {
   private Pessoa[] list;
-  
+
   public Agenda() {
     this.list = new Pessoa[10];
   }
 
   public void add(Pessoa person) {
-    if(isFull(list)) return;
+    if (isFull(list)) {
+      System.out.printf("Agenda está cheia para adicionar %s\n", person.getName());
+      return;
+    }
 
     for (int i = 0; i < list.length; i++) {
-      if(list[i] == null) {
+      if (list[i] == null) {
+        System.out.printf("Adicionando %s\n", person.getName());
         list[i] = person;
         break;
       }
@@ -19,26 +23,69 @@ public class Agenda {
   }
 
   public void remove(Pessoa person) {
-    if(person == null) return;
+    if (person == null)
+      return;
 
     for (int i = 0; i < list.length; i++) {
-      if(person.equals(list[i])) list[i] = null;
+      if (person.equals(list[i]))
+        list[i] = null;
     }
   }
 
   public int includes(Pessoa person) {
-    if(person == null) return -1; // Null guard clause
+    if (person == null)
+      return -1; // Null guard clause
 
     for (int i = 0; i < list.length; i++) {
-      if(person.equals(list[i])) return i;
+      if (person.equals(list[i]))
+        return i + 1;
     }
 
     return 0;
   }
 
+  public void searchPhoneByName(String name) {
+    if (name == null || name.isBlank())
+      return;
+
+    int found = 0;
+
+    for (Pessoa person : list) {
+      if (person == null)
+        continue;
+
+      if (person.getName().toLowerCase().contains(name.toLowerCase())) {
+        found++;
+        System.out.printf("Name: %s, Phone: %s\n", person.getName(), person.getPhone());
+      }
+    }
+
+    System.out.printf("%d pessoa(s) encontrada(s)\n", found);
+  }
+
+  public void searchEmailByName(String name) {
+    if (name == null || name.isBlank())
+      return;
+
+    int found = 0;
+
+    for (Pessoa person : list) {
+      if (person == null)
+        continue;
+
+      if (person.getName().toLowerCase().contains(name.toLowerCase())) {
+        found++;
+        System.out.printf("Name: %s, Phone: %s\n", person.getName(), person.getEmail());
+      }
+    }
+
+    System.out.printf("%d pessoa(s) encontrada(s)\n", found);
+  }
+
   private static boolean isFull(Pessoa[] list) {
     for (Pessoa person : list) {
-      if(person == null) return false;
+      if (person == null)
+        return false;
     }
 
     return true;
